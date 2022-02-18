b8f083e4eb4c311dff0775c89ebf365abc9ca702
Ian <ian.lommock@gmail.com>
2022-02-18 11:42:32 -0500

    - created initialize() to set up the data structures and call connect - added exception handling to make_query() - changed output of make_query() to be a list, instead of the cursor object

--- database_handler.py
+++ database_handler.py
@@ -1,42 +1,56 @@
 import pymysql
 
+import soupbot_utilities as util
+
 conn = None
 cursor = None
 
 activeConnection = False
 
-
-def isConnected():
-    return activeConnection
+user, password, host, db = None, None, None, None
 
 
-def connect(username, password, host, db):
+def initialize(u, p, h, d):
+    global user, password, host, db
+    user, password, host, db = u, p, h, d
+    connect()
+
+
+def connect():
     global conn, cursor, activeConnection
+    global user, password, host, db
+    util.timed_message("Trying to connect to mysql...")
     if not activeConnection:
-        conn = pymysql.connect(user=username, password=password, host=host, database=db)
+        conn = pymysql.connect(user=user, password=password, host=host, database=db)
         conn.autocommit(True)
         cursor = conn.cursor()
         activeConnection = True
-        print("Connection to mysql successful")
+        util.timed_message("Connection successful")
     else:
-        print("Error: Connection to mysql rejected")
+        util.timed_message("Error: Already connected")
 
 
 def disconnect():
     global conn, cursor, activeConnection
+    util.timed_message("Trying to disconnect from mysql...")
     if activeConnection:
         cursor.close()
         conn.close()
         activeConnection = False
-        print("Disconnection from mysql successful")
+        util.timed_message("Disconnection successful")
     else:
-        print("Error: Disconnection from mysql rejected")
+        util.timed_message("Error: Not connected")
 
 
 def make_query(query, values=tuple()):
-    global cursor
-    if len(values) > 0:
-        cursor.execute(query, values)
-    else:
-        cursor.execute(query)
-    return cursor
+    global cursor, activeConnection
+    try:
+        if len(values) > 0:
+            cursor.execute(query, values)
+        else:
+            cursor.execute(query)
+    except BrokenPipeError:
+        util.timed_message(str(BrokenPipeError))
+        disconnect()
+        connect()
+    return [c for c in cursor]
