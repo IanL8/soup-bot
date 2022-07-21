ac5f57c29b32c6a2180cccb5fa2fe5cb9da279bc
Ian <ian.lommock@gmail.com>
2022-07-21 13:44:06 -0400

    sped up the download of playlists and fixed the issue where long playlists would break the bot

--- vc_commands.py
+++ vc_commands.py
@@ -2,7 +2,9 @@
 # imports
 import discord
 import asyncio
-import youtube_dl
+import threading
+import yt_dlp
+from pytube import Playlist as YTPlaylist
 import os
 from dotenv import load_dotenv
 
@@ -23,13 +25,11 @@ FFMPEG_OPTIONS = {"before_options": "-reconnect 1 -reconnect_streamed 1 -reconne
 
 YDL_OPTIONS = {  # taken from https://github.com/Rapptz/discord.py/blob/master/examples/basic_voice.py
     'format': 'bestaudio/best',
-    # 'username': USERNAME,
-    # 'password': PASSWORD,
-    # "cookies": "src/youtube.com_cookies.txt",
-    "agelimit": 30,
+    'postprocessors': [{  # Extract audio using ffmpeg
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'm4a',
+    }],
     'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-    'restrictfilenames': True,
-    'noplaylist': False,
     'nocheckcertificate': True,
     'ignoreerrors': False,
     'logtostderr': False,
@@ -74,6 +74,45 @@ async def play_next_helper(song, loop, guild, channel, vc):
     vc.play(source, after=lambda err: play_next(loop, guild, channel, vc))
 
 #
+# download queue
+# idea: https://stackoverflow.com/a/26270790
+
+downloadQueue = list()
+
+stopThread = False
+
+def loop_in_thread(loop):
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(download_queue_processor())
+    return
+
+async def download_queue_processor():
+    while True:
+        await asyncio.sleep(10)
+        if stopThread:
+            return
+        if downloadQueue:
+            queue, loop, guild, channel = downloadQueue.pop(0)
+            with yt_dlp.YoutubeDL(YDL_OPTIONS) as ydl:
+                while queue:
+                    url = queue.pop(0)
+                    try:
+                        info = ydl.extract_info(url, download=False)
+                        playlists[guild].queue.append(Song(info["url"], info["title"]))
+                    except yt_dlp.DownloadError as derr:
+                        util.soup_log("[ERROR] {}".format(derr.args))
+                        asyncio.run_coroutine_threadsafe(channel.send(f"error: invalid link on {url}"), loop)
+
+downloadLoop = asyncio.new_event_loop()
+t = threading.Thread(target=loop_in_thread, args=(downloadLoop,))
+t.start()
+
+def close():
+    global stopThread
+    stopThread = True
+
+
+#
 # bot commands
 
 # join vc
@@ -102,6 +141,11 @@ async def leave(context):
 #credit: https://www.youtube.com/watch?v=jHZlvRr9KxM
 @commandHandler.command("play", "play a given youtube video (link) in vc")
 async def play(context):
+    # if not a youtube link
+    url = util.list_to_string(context.args, "")
+    if not "https://www.youtube.com/" in url:
+        return await context.channel.send("not a youtube link")
+
     # if bot not in voice
     if not context.guild.voice_client in context.bot.voice_clients:
 
@@ -111,29 +155,27 @@ async def play(context):
         else:
             return await context.channel.send("user is not in a voice channel")
 
-    url = util.list_to_string(context.args, "")
     vc = context.guild.voice_client # can't use shortcut
 
     # create a playlist for context.guild if one does not exist already
     if not playlists.get(context.guild):
         playlists[context.guild] = Playlist()
 
-    with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+    with yt_dlp.YoutubeDL(YDL_OPTIONS) as ydl:
+
+        if "https://www.youtube.com/playlist?list=PL" in url:
+            queue = list()
+            for u in YTPlaylist(url).video_urls:
+                queue.append(u)
+            url = queue.pop(0)
+            downloadQueue.append((queue, context.bot.loop, context.guild, context.channel))
+
         try:
             info = ydl.extract_info(url, download=False)
-        except youtube_dl.DownloadError as e1:
-            util.soup_log("[ERROR] {}".format(e1.args))
+            playlists[context.guild].queue.append(Song(info["url"], info["title"]))
+        except yt_dlp.DownloadError as derr:
+            util.soup_log("[ERROR] {}".format(derr.args))
             return await context.channel.send("invalid link")
-
-        if info.get("_type") == "playlist":
-            i = 0
-            for e in info["entries"]:
-                playlists[context.guild].queue.append(Song(e["formats"][0]["url"], e["title"]))
-                i += 1
-            await context.channel.send(f"added {i} videos to queue")
-        else:
-            playlists[context.guild].queue.append(Song(info["formats"][0]["url"], info["title"]))
-            await context.channel.send("added 1 video to queue")
 
         if not vc.is_playing():
 
@@ -145,8 +187,6 @@ async def play(context):
                 vc.play(source, after=(lambda err: play_next(context.bot.loop, context.guild, context.channel, vc)))
             except discord.errors.ClientException:
                 playlists[context.guild].queue.insert(0, song)
-
-
 
 # pause vid
 @commandHandler.command("pause", "pause the current video")
