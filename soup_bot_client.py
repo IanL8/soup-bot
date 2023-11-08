fb8aa835cb0f693729fbb7515949caa778f2d66c
Ian <ian.lommock@gmail.com>
2023-11-07 20:51:59 -0500

    cleaned up formatting, made minor changes to the code for readability, and adjusted for a change in the CommandHandler object

--- soup_bot_client.py
+++ soup_bot_client.py
@@ -1,61 +1,53 @@
-#
-# imports
 import discord
 
-#
-# project imports
-from command_management.context import Context
-from command_management.command_handler import CommandHandler
+from command_management import CommandHandler, Context
 from command_blocks import *
 import database_handler as db
 import soupbot_utilities as util
 
-#
-# soupbot
+
 class SoupBotClient(discord.Client):
-    # init
+
     def __init__(self, **options):
         super().__init__(**options)
-        # init tables
-        db.init()
 
-        # init cmds
+        if not db.make_tables():
+            util.soup_log("[DBS] failed to create tables")
+
+        # initialize command handler and add blocks
         self.cmd_handler = CommandHandler()
+        for b in [general_commands.Block, time_commands.Block, vc_commands.Block]:
+            self.cmd_handler.add_block(b())
 
-        cmd_blocks = [general_commands.GeneralCommands(), time_commands.TimeCommands(), vc_commands.MusicCommands()]
-        for b in cmd_blocks:
-            self.cmd_handler.add_block(b)
-
-    # on connection to discord
     async def on_ready(self):
+        # add guilds to database
         for g in self.guilds:
             if not db.add_guild(g):
                 util.soup_log(f"[ERROR] {g.name} could not be added to the db")
-        util.soup_log(f"[BOT] {self.user.name} has connected to Discord")
+
+        # sync command tree
         try:
             await self.cmd_handler.make_command_tree(self).sync()
         except discord.errors.ClientException:
             util.soup_log("[BOT] failed to create command tree")
 
-    # on close
+        util.soup_log(f"[BOT] {self.user.name} has connected to Discord")
+
     async def close(self):
         for vc in self.voice_clients:
-            await vc.disconnect(force=False)
+            await vc.disconnect(force=True)
 
         self.cmd_handler.close()
         util.soup_log(f"[BOT] {self.user.name} has disconnected from Discord")
 
-    # on guild join
     async def on_guild_join(self, guild):
         if not db.add_guild(guild):
             util.soup_log(f"[ERROR] {guild.name} could not be added to the db")
 
-    # on member join
     async def on_member_join(self, member):
         if not db.add_member(member.id, member.guild.id):
             util.soup_log(f"[ERROR] {member.name}:{member.id} could not be added to the db")
 
-    # on message
     async def on_message(self, message):
         if message.author == self.user or len(message.content) == 0:
             return
@@ -63,9 +55,11 @@ class SoupBotClient(discord.Client):
         if db.get_flag(message.guild.id) == message.content[0]:
             args = message.content.split(" ")
             cmd = args.pop(0)[1:]
-            if self.cmd_handler.is_command(cmd):
-                c = Context(message, message.channel, message.author, message.guild, message.guild.voice_client, self, args)
+
+            index = self.cmd_handler.find_block(cmd)
+
+            if index != -1:
                 try:
-                    await self.cmd_handler.pass_command(cmd, c)
+                    await self.cmd_handler.pass_command(index, cmd, Context(message, self, args))
                 except discord.errors.HTTPException:
                     await message.channel.send("content too large")
