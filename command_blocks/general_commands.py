84e574e45e629bb1258f0e64382dcc2be1cc27af
Ian <ian.lommock@gmail.com>
2023-11-07 20:49:57 -0500

    cleaned up formatting and improved avatar command for new API update

--- command_blocks/general_commands.py
+++ command_blocks/general_commands.py
@@ -1,29 +1,25 @@
-#
-# imports
 import random
 
-#
-# project imports
-from command_management.commands import Commands, CommandBlock
+from command_management import Commands, CommandBlock
 import database_handler as db
 import soupbot_utilities as util
 
 
-class GeneralCommands(CommandBlock):
+class Block(CommandBlock):
 
     name = "general commands"
-    commands = Commands()
+    cmds = Commands()
 
-    @commands.command("hello")
+    @cmds.command("hello")
     async def hello(self, context):
         await context.send_message(f"hi {context.author.display_name}")
 
-    @commands.command("true")
+    @cmds.command("true")
     async def truer(self, context):
-        msg = ("TRUE" if random.random() > .49 else "NOT FALSE") + " <:LULW:1010200083314257960>"
+        msg = ("TRUE" if random.random() > .49 else "NOT FALSE")
         await context.send_message(msg)
 
-    @commands.command("roll", info="roll a dice with the given amount of sides [default 100]", enable_input=True)
+    @cmds.command("roll", info="roll a dice with the given amount of sides [default 100]", enable_input=True)
     async def roll(self, context):
         k = 100
         if len(context.args) > 0 and context.args[0].isdigit() and int(context.args[0]) != 0:
@@ -35,11 +31,11 @@ class GeneralCommands(CommandBlock):
 
         await context.send_message(num)
 
-    @commands.command("word", "random word")
+    @cmds.command("word", "random word")
     async def word(self, context):
         await context.send_message(util.WORD_LIST[int(random.random() * len(util.WORD_LIST))])
 
-    @commands.command("phrase", "random string of words of a given size [default 2 | max 100]")
+    @cmds.command("phrase", "random string of words of a given size [default 2 | max 100]")
     async def phrase(self, context):
         k = 2
         if len(context.args) > 0 and context.args[0].isdigit() and 0 != int(context.args[0]) < 101:
@@ -50,7 +46,7 @@ class GeneralCommands(CommandBlock):
 
         await context.send_message(util.list_to_string(temp, " "))
 
-    @commands.command("8ball", enable_input=True)
+    @cmds.command("8ball", enable_input=True)
     async def magic_8Ball(self, context):
         msg = ""
         if not context.basic:
@@ -63,7 +59,7 @@ class GeneralCommands(CommandBlock):
 
         await context.send_message(msg)
 
-    @commands.command("lookup", "look up a given league player on op.gg", enable_input=True)
+    @cmds.command("lookup", "look up a given league player on op.gg", enable_input=True)
     async def lookup(self, context):
         if len(context.args) == 0:
             msg = "no name specified"
@@ -72,7 +68,7 @@ class GeneralCommands(CommandBlock):
 
         await context.send_message(msg)
 
-    @commands.command("which", "pick between a given set of options (separated by commas)", enable_input=True)
+    @cmds.command("which", "pick between a given set of options (separated by commas)", enable_input=True)
     async def which(self, context):
         tempList = [s.strip() for s in util.list_to_string(context.args).split(",")]
         while "" in tempList:
@@ -84,40 +80,32 @@ class GeneralCommands(CommandBlock):
 
         await context.send_message(msg)
 
-    @commands.command("git")
+    @cmds.command("git")
     async def git(self, context):
         await context.send_message("https://github.com/IanL8/soup-bot")
 
-    @commands.command("avatar", "fetch a user's profile picture", enable_input=True)
+    @cmds.command("avatar", "fetch a user's profile picture", enable_input=True)
     async def get_avatar(self, context):
-        name = util.list_to_string(context.args, " ")
-        i = None
 
         if len(context.args) == 0:
-            return await context.send_message(context.author.avatar)
-
-        for member in context.guild.members:
-            if str(member.nick).lower() == name.lower() or str(member.name).lower() == name.lower():
-                i = member.id
-
-        if i:
-            msg = str(context.guild.get_member(i).avatar)
+            msg = str(context.author.avatar)
+        elif len(context.mentions) > 0:
+            msg = str(context.mentions[0].avatar)
         else:
-            msg = "invalid nickname"
+            msg = "no user found"
 
         await context.send_message(msg)
 
     #
     # database commands
 
-    @commands.command("fortune", "get a random fortune once per day")
+    @cmds.command("fortune", "get a random fortune once per day")
     async def fortune(self, context):
         uid = context.author.id
 
         await context.send_message(db.get_fortune(uid))
 
-
-    @commands.command("add", "add a movie to the list", enable_input=True)
+    @cmds.command("add", "add a movie to the list", enable_input=True)
     async def add_movie(self, context):
         gid = context.guild.id
 
@@ -131,7 +119,7 @@ class GeneralCommands(CommandBlock):
 
         await context.confirm()
 
-    @commands.command("remove", "remove a movie from the list", enable_input=True)
+    @cmds.command("remove", "remove a movie from the list", enable_input=True)
     async def remove_movie(self, context):
         gid = context.guild.id
 
@@ -145,7 +133,7 @@ class GeneralCommands(CommandBlock):
 
         await context.confirm()
 
-    @commands.command("movies", "list all movies")
+    @cmds.command("movies", "list all movies")
     async def movie_list(self, context):
         li = db.get_movie_list(context.guild.id)
         if not li:
@@ -161,7 +149,7 @@ class GeneralCommands(CommandBlock):
     #
     # admin commands
 
-    @commands.command("changeprefix", "change the prefix that the bot is accessed with", enable_input=True)
+    @cmds.command("changeprefix", "change the prefix that the bot is accessed with", enable_input=True)
     async def set_flag(self, context):
         if len(context.args) == 0 or len(context.args[0]) < 0 or len(context.args[0]) > 2:
             return await context.send_message("bad prefix")
