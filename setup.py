a6bf862e69064a8037cef40267dbdd2e0d2827e6
Ian <ian.lommock@gmail.com>
2023-11-07 20:50:15 -0500

    cleaned up formatting and pull env constants from util now

--- setup.py
+++ setup.py
@@ -1,17 +1,8 @@
-#
-# imports
-from os import getenv
-from dotenv import load_dotenv
 from discord import Intents
 
-#
-# project imports
 from soup_bot_client import SoupBotClient
+from soupbot_utilities import TOKEN
 
-#
-# get .env values
-load_dotenv("values.env")
-TOKEN = getenv("DISCORD_TOKEN")
 
 if __name__ == "__main__":
     intents = Intents.all()
