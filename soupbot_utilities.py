b0f431628822be0fcb0f41348315f628c2f58d6f
Ian <ian.lommock@gmail.com>
2024-02-18 18:14:52 -0500

    removed find_in_list, list_to_string, added env var for spotipy

--- soupbot_utilities.py
+++ soupbot_utilities.py
@@ -5,16 +5,9 @@ from dotenv import load_dotenv
 #
 # functions
 
-# find val in a 2D list
-def find_in_list(val, li):
-    for elements in li:
-        for e in elements:
-            if val == e:
-                return True
-    return False
+_TIME_UNITS = [(86400, "days"), (3600, "hours"), (60, "min"), (1, "sec")]
+_DATE_STRING = "{month:02}/{day:02}: {hour:02}:{minute:02}:{sec:02}"
 
-
-# creates a list of strings from a txt file
 def read_file_to_list(src):
     temp = []
     f = open(src)
@@ -25,23 +18,11 @@ def read_file_to_list(src):
     f.close()
     return temp
 
-
-# creates a string from a list with sep between the elements
-def list_to_string(li, sep=" "):
-    temp = ""
-    for i, e in enumerate(li):
-        if i < len(li)-1:
-            temp += e.strip() + sep
-        else:
-            temp += e
-    return temp
-
-
-# takes a time t in seconds and gives the time in days, hours, min, sec
 def time_to_string(t):
-    timeUnits = [(86400, "days"), (3600, "hours"), (60, "min"), (1, "sec")]
+    """Takes a time t in seconds and returns the time in days, hours, min, sec"""
+
     s = ""
-    for i, j in timeUnits:
+    for i, j in _TIME_UNITS:
         temp = int(t / i)
         t = t % i
         if temp > 0:
@@ -50,16 +31,14 @@ def time_to_string(t):
     return s[:-2]
 
 
-# returns the current date and time
-def get_date():
+def _get_date():
     t = time.localtime()
-    return "{month:02}/{day:02}: {hour:02}:{minute:02}:{sec:02}"\
-        .format(month=t[1], day=t[2], hour=t[3], minute=t[4], sec=t[5])
+    return _DATE_STRING.format(month=t[1], day=t[2], hour=t[3], minute=t[4], sec=t[5])
 
 
-# prints a message s with a timestamp
 def soup_log(s):
-    print(f"{get_date()}: {s}")
+    """prints a message s with a timestamp"""
+    print(f"{_get_date()}: {s}")
 
 #
 # constants
@@ -68,6 +47,8 @@ def soup_log(s):
 load_dotenv("values.env")
 TOKEN = getenv("DISCORD_TOKEN")
 FFMPEG_EXE = getenv("FFMPEG_EXE")
+SPOTIPY_CLIENT_SECRET = getenv("SPOTIPY_CLIENT_SECRET")
+SPOTIPY_CLIENT_ID = getenv("SPOTIPY_CLIENT_ID")
 
 FFMPEG_OPTIONS = {"before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5", "options": "-vn"}
 
@@ -83,7 +64,8 @@ YDL_OPTIONS = {  # referenced from https://github.com/Rapptz/discord.py/blob/mas
     'logtostderr': False,
     'quiet': True,
     'no_warnings': True,
-    'default_search': 'auto',
+    # 'default_search': 'auto',
+    # 'flat_playlist': True,
     'source_address': '0.0.0.0',  # bind to ipv4 since ipv6 addresses cause issues sometimes
 }
 
