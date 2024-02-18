6cbfb3e373714b496deef66729d0986848dcae36
Ian <ian.lommock@gmail.com>
2024-02-18 18:12:01 -0500

    Renamed basic to is_basic.  Renamed cmds dict to basic, and app_cmds dict to app and made it a list

--- command_management.py
+++ command_management.py
@@ -5,56 +5,54 @@ import soupbot_utilities as util
 
 class Context:
 
-    def __init__(self, args, message : Message = None, interaction : Interaction = None, bot : Client = None):
+    def __init__(self, args, message : Message=None, interaction:Interaction=None, bot:Client=None):
 
         if interaction is None:
-            self.__interaction = None
-            self.__message = message
-            self.basic = True # basic cmd: True, app cmd: False
+            self._interaction = None
+            self._message = message
+            self.is_basic = True
             self.bot = bot
             self.args = args
             self.channel = message.channel
             self.author = message.author
             self.guild = message.guild
             self.mentions = message.mentions
-            self.voice_client = message.guild.voice_client
         else:
-            self.__interaction = interaction
-            self.__message = None
-            self.basic = False
+            self._interaction = interaction
+            self._message = None
+            self.is_basic = False
             self.bot = interaction.client
             self.args = args
             self.channel = interaction.channel
             self.author = interaction.user
             self.guild = interaction.guild
             self.mentions = []
-            self.voice_client = interaction.guild.voice_client
 
             if "resolved" in interaction.data.keys():
                 self.mentions = [self.guild.get_member(int(i)) for i in interaction.data["resolved"]["users"].keys()]
 
     async def send_message(self, s):
-        if self.basic:
+        if self.is_basic:
             msg = await self.channel.send(s)
         else:
-            await self.__interaction.response.send_message(s)
-            msg = await self.__interaction.original_response()
+            await self._interaction.response.send_message(s)
+            msg = await self._interaction.original_response()
 
         return msg
 
     async def confirm(self):
-        if self.basic:
-            await self.__message.add_reaction("✅")
+        if self.is_basic:
+            await self._message.add_reaction("✅")
         else:
-            await self.__interaction.response.send_message("✅")
+            await self._interaction.response.send_message("✅")
 
 
 class Commands:
 
     def __init__(self):
-        self.cmds = dict()      # cmd name -> function pointer
-        self.app_cmds = dict()  # cmd name -> function pointer
-        self.block = None
+        self.basic = dict() # cmd name -> function pointer
+        self.app = []
+        self.block: CommandBlock = None
 
     # blocks can't be created until after Commands objects, so must be added in a setter
     def set_block(self, block):
@@ -71,29 +69,32 @@ class Commands:
 
             # app wrapper
             if enable_input:
-                async def app_wrapper(interaction: Interaction, enter: str):
-                    util.soup_log(f"[CMD] {name} {enter if not enter else enter.split()}")
-                    return await f(self.block, Context(enter.split(" "), interaction=interaction))
+                async def app_wrapper(interaction: Interaction, enter:str=None):
+                    args = []
+                    if enter: args.extend(enter.split(" "))
+                    util.soup_log(f"[CMD] {name} {args if args else str()}")
+                    return await f(self.block, Context(args, interaction=interaction))
             else:
                 async def app_wrapper(interaction: Interaction):
                     util.soup_log(f"[CMD] {name}")
                     return await f(self.block, Context([], interaction=interaction))
 
-            self.cmds[name] = basic_wrapper
-            self.app_cmds[name] = app_commands.command(name=name, description=info)(app_wrapper)
+            self.basic[name] = basic_wrapper
+            self.app.append(app_commands.command(name=name, description=info)(app_wrapper))
 
             return basic_wrapper
-
         return decorator
 
 
 class CommandBlock:
+    """meant to be subclassed in order to create commands for the discord bot"""
 
     name: str = "default"
-    cmds : Commands # each block subclass needs to have its own copy of cmds
+    commands : Commands # each block subclass needs to make its own instance of Commands
 
     def __init__(self):
-        self.cmds.set_block(self)
+        # the commands object needs the block instance in order for its methods to be callable by the discord client
+        self.commands.set_block(self)
 
     def close(self):
         pass
@@ -106,13 +107,13 @@ class CommandHandler:
 
     def add_block(self, block: CommandBlock):
         self.blocks.append(block)
-        util.soup_log(f"[BLK] block: {block.name} added")
+        util.soup_log(f"[BLK] block : {block.name} added")
 
     def make_command_tree(self, client):
         tree = app_commands.CommandTree(client)
         for b in self.blocks:
-            for v in b.cmds.app_cmds.values():
-                tree.add_command(v)
+            for a in b.commands.app:
+                tree.add_command(a)
 
         return tree
 
@@ -122,7 +123,7 @@ class CommandHandler:
 
     def find_block(self, c):
         for i, b in enumerate(self.blocks):
-            if c in b.cmds.cmds.keys():
+            if c in b.commands.basic.keys():
                 return i
 
         return -1
@@ -130,7 +131,8 @@ class CommandHandler:
     def list_commands(self):
         c = []
         for b in self.blocks:
-            c.extend(b.cmds.cmds.keys())
+            c.extend(b.commands.basic.keys())
+        return c
 
-    def pass_command(self, block_index, c, context: Context):
-        return self.blocks[block_index].cmds.cmds[c](context)
+    def pass_command(self, block_index, cmd, context: Context):
+        return self.blocks[block_index].commands.basic[cmd](context)
