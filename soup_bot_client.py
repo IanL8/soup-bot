c007a56f98947a4fc2d10da88210e271e4b02cad
Ian <ian.lommock@gmail.com>
2024-03-06 19:02:13 -0500

    Minor formatting+structure changes

--- soup_bot_client.py
+++ soup_bot_client.py
@@ -1,54 +1,65 @@
 import discord
 
-from command_management import CommandHandler, Context
-from command_blocks import *
-import database_handler as db
-import soupbot_utilities as util
+from command_management.context import Context
+from command_management.command_handler import CommandHandler
+from command_lists import general, music, timer
+import database_handler as database
+from soupbot_util.logger import soup_log
 
 
 class SoupBotClient(discord.Client):
 
     def __init__(self, **options):
         super().__init__(**options)
-
-        self.cmd_handler = CommandHandler()
-        for cmd_module in [general, music, timer]:
-            self.cmd_handler.add_block(cmd_module.Block())
+        self.command_handler = CommandHandler()
+        command_lists = [general.GeneralCommandList(), music.MusicCommandList(), timer.TimerCommandList()]
+        for command_list in command_lists:
+            self.command_handler.add_command_list(command_list)
 
     async def on_ready(self):
-        for g in self.guilds:
-            db.add_guild(g.id, g.text_channels[0].id, g.owner_id, [m.id for m in g.members if not m.bot])
-
+        for guild in self.guilds:
+            database.add_guild(
+                guild.id,
+                guild.text_channels[0].id,
+                guild.owner_id,
+                [member.id for member in guild.members if not member.bot]
+            )
         try:
-            await self.cmd_handler.make_command_tree(self).sync()
+            await self.command_handler.make_command_tree(self).sync()
         except discord.errors.ClientException:
-            util.soup_log("[BOT] failed to create command tree")
-
-        util.soup_log(f"[BOT] {self.user.name} has connected to Discord")
+            soup_log("failed to create command tree", "bot")
+        soup_log(f"{self.user.name} has connected to Discord", "bot")
 
     async def close(self):
         for vc in self.voice_clients:
             await vc.disconnect(force=True)
-
-        self.cmd_handler.close()
-        util.soup_log(f"[BOT] {self.user.name} has disconnected from Discord")
+        self.command_handler.on_close()
+        soup_log(f"{self.user.name} has disconnected from Discord", "bot")
 
     async def on_guild_join(self, guild):
-        db.add_guild(guild.id, guild.text_channels[0].id, guild.owner_id, [m.id for m in guild.members if not m.bot])
+        database.add_guild(
+            guild.id,
+            guild.text_channels[0].id,
+            guild.owner_id,
+            [m.id for m in guild.members if not m.bot]
+        )
 
     async def on_member_join(self, member):
-        db.add_member(member.id, member.guild.id)
+        database.add_member(member.id, member.guild.id)
 
     async def on_message(self, message):
-        if not (message.author == self.user or len(message.content) == 0) \
-                and db.get_prefix(message.guild.id) == message.content[0]:
-
-            args = message.content.split(" ")
-            cmd = args.pop(0)[1:]
-            block_index = self.cmd_handler.find_block(cmd)
-
-            if block_index != -1:
-                try:
-                    await self.cmd_handler.pass_command(block_index, cmd, Context(args, message=message, bot=self))
-                except discord.errors.HTTPException:
-                    await message.channel.send("content too large")
+        prefix = database.get_prefix(message.guild.id)
+        if len(message.content) == 0 or prefix != message.content[0] or message.author == self.user:
+            return
+        command_name = message.content.split(" ").pop(0)[1:]
+        index = self.command_handler.find_command_list_index(command_name)
+        if index == -1:
+            return
+        try:
+            await self.command_handler.pass_command(
+                index,
+                command_name,
+                Context(message.content[len(prefix)+len(command_name):].strip(), message=message, bot=self)
+            )
+        except discord.errors.HTTPException:
+            await message.channel.send("content too large")
