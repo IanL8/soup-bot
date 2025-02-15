290c180032e11ead6bf45ddda01080ef352a9771
Ian <ian.lommock@gmail.com>
2025-02-14 22:12:57 -0500

    Switched translation source from deep_translator to using a mix of deepl and Microsoft Azure AI translator. Moved the translator command into its own file. Added support for autocomplete options for fields

---
+++ command_lists/translator.py
@@ -0,0 +1,51 @@
+import deepl
+
+from command_management import commands
+from soupbot_util.constants import  DEEPL_API_KEY, DEEPL_TRANSLATION_KEY, AZURE_TRANSLATION_KEY
+
+from ._azure_translator import azure_translate, azure_detect
+
+deepl_translator = deepl.Translator(DEEPL_API_KEY)
+
+
+class CommandList(commands.CommandList):
+
+    name = "translator commands"
+
+    def on_close(self):
+        deepl_translator.close()
+
+    @commands.command("translate",
+                      desc="Translates some provided text. Can optionally supply languages to translate to or from",
+                      autocomplete_fields={"from_language": AZURE_TRANSLATION_KEY.keys(),
+                                           "to_language": AZURE_TRANSLATION_KEY.keys()}
+                      )
+    async def translate_text(self, context, text: str, from_language: str = "auto", to_language: str = "english"):
+        # deepl has better translations, but has bad auto-detection and worse coverage than azure
+        # this method uses azure for detection and deepl for translations
+        # if deepl does not have the language, azure is used as a backup
+
+        if to_language.lower() not in DEEPL_TRANSLATION_KEY["target"].keys() \
+                or (from_language.lower() != "auto" and from_language.lower() not in DEEPL_TRANSLATION_KEY[
+            "source"].keys()):
+            await context.send_message(azure_translate(text, from_language, to_language, ))
+            return
+
+        if from_language == "auto":
+            source_lang = azure_detect(text)
+
+            if source_lang is None:
+                await context.send_message("not able to generate a translation")
+                return
+            elif source_lang not in DEEPL_TRANSLATION_KEY["source"].values():
+                await context.send_message(azure_translate(text, from_language, to_language))
+                return
+
+        else:
+            source_lang = DEEPL_TRANSLATION_KEY["source"][from_language.lower()]
+
+        await context.send_message(deepl_translator.translate_text(
+            text,
+            source_lang=source_lang,
+            target_lang=DEEPL_TRANSLATION_KEY["target"][to_language.lower()]
+        ))
--- command_management/commands.py
+++ command_management/commands.py
@@ -7,17 +7,17 @@ from .context import Context
 
 
 class _CommandDataWrapper:
-    def __init__(self, name:str, desc:str, parameters:str, args:str):
+    def __init__(self, name:str, desc:str, autocomplete_fields:{str:list[str]}, parameters:str, args:str):
         self.name = name
         self.desc = desc
+        self.autocomplete_fields = autocomplete_fields
         self.parameters = parameters
         self.args = args
 
 
 _all_commands: {callable:_CommandDataWrapper} = dict()
 
-
-def command(name:str, desc:str="..."):
+def command(name:str, desc:str="...", autocomplete_fields:{str:list[str]}= None):
     """Decorator for methods defining a discord bot command. Command must have the parameter 'context', be asynchronous, and
     be an instance method to a subclass of 'CommandList' in order to work correctly. Any parameters after context will be
     given as fields for the command and must be given a type."""
@@ -30,7 +30,7 @@ def command(name:str, desc:str="..."):
         args = str(inspect.getfullargspec(f).args)
         args = args[args.index("'context'") + 10:-1].replace("'", "").strip()
 
-        _all_commands[f] = _CommandDataWrapper(name, desc, parameters, args)
+        _all_commands[f] = _CommandDataWrapper(name, desc, autocomplete_fields, parameters, args)
         return f
 
     return decorator
@@ -45,22 +45,33 @@ class CommandList(ABC):
 
         for value in self.__class__.__dict__.values():
             if value in _all_commands.keys():
-                cmd = _all_commands.pop(value)
+                command_data = _all_commands.pop(value)
 
-                self.app_commands.append(self._generate_app_command(cmd.name, cmd.desc, cmd.parameters, cmd.args, value))
+                self.app_commands.append(self._generate_app_command(command_data, value))
 
-    def _generate_app_command(self, name, desc, parameters, args, function):
-        cmd_name_llll = name # avoid variable name overlap with variables in parameter/args strings
+    def _generate_app_command(self, command_data, function):
         scope = dict(locals().copy(), **globals().copy())
 
         exec(
-            f"async def app_wrapper(interaction:Interaction, {parameters}): "
-                f"soup_log(cmd_name_llll + ' ' + str(({args})), 'cmd');"
-                f"return await function(self, Context(interaction=interaction), {args});",
+            f"@app_commands.command(name=command_data.name, description=command_data.desc[:100])\n"
+            f"async def app_wrapper(interaction:Interaction, {command_data.parameters}): "
+                f"soup_log(command_data.name + ' ' + str(({command_data.args})), 'cmd');"
+                f"return await function(self, Context(interaction=interaction), {command_data.args});",
             scope
         )
 
-        return app_commands.command(name=name, description=desc[:100])(scope["app_wrapper"])
+        if command_data.autocomplete_fields is not None:
+            for field_name, choices in command_data.autocomplete_fields.items():
+                scope["field_name"] = field_name
+                scope["choices"] = choices
+                exec(
+                    f"@app_wrapper.autocomplete(field_name)\n"
+                    f"async def field_name_autocomplete(interaction:Interaction, current:str) -> list[app_commands.Choice[str]]: "
+                    f"return [app_commands.Choice(name=c, value=c) for c in choices if current.lower() in c.lower()][:25]",
+                    scope
+                )
+
+        return scope["app_wrapper"]
 
     @abstractmethod
     def on_close(self):
