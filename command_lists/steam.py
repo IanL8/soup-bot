40d5cd34ffc0fd037f5f2d37c8e79364e35ba99c
Ian <ian.lommock@gmail.com>
2025-04-17 15:16:59 -0400

    made improvements to the player count command

--- command_lists/steam.py
+++ command_lists/steam.py
@@ -4,9 +4,9 @@ from time import sleep, time
 import re
 
 from command_management import commands
-from database.database_management import db_steam_apps, db_steam_app_table_refreshes
+from database.database_management import db_steam_apps
 from soupbot_util import constants
-from soupbot_util.logger import soup_log
+
 
 _is_running = True
 
@@ -38,12 +38,10 @@ def _get_apps():
     return temp_apps
 
 
-def _refresh(apps, start_time):
+def _fill_table(apps):
 
     def search_prio(pc):
         return float(pc) / 100000.0
-
-    progress_display = [.1, .2, .3, .4, .5, .6, .7, .8, .9, .95, .96, .97, .98, .99, 1]
 
     for i, app in enumerate(apps):
 
@@ -51,71 +49,49 @@ def _refresh(apps, start_time):
             f"https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid={app['appid']}&format=json")
         response = request.json()
 
-        if "player_count" in response["response"]:
-            db_steam_apps.add_or_update(
-                app["appid"],
-                app["name"],
-                _make_searchable_name(app["name"]),
-                search_prio(response['response']['player_count'])
-            )
+        db_steam_apps.add_or_update(
+            app["appid"],
+            app["name"],
+            _make_searchable_name(app["name"]),
+            -1 if not "player_count" in response["response"] else search_prio(response['response']['player_count']),
+            int(time())
+        )
 
-            db_steam_app_table_refreshes.update_last_app_id(start_time, app["appid"])
-
-        while len(progress_display) > 0 and progress_display[0] < float(i+1) / len(apps):
-            soup_log(f"Steam app processing {int(progress_display.pop(0) * 100)}% of the way done", "APP")
-
-    db_steam_app_table_refreshes.mark_refresh_complete(start_time)
-    soup_log(f"Steam apps search priority update finished", "APP")
 
 def _background_apps_refresh(is_running):
-    four_months = 10713600
     half_hour = 1800
 
     sleep(7) # wait for tables to be created in main thread
 
     if db_steam_apps.is_empty():
-
-        start_time = int(time())
-        db_steam_app_table_refreshes.add(start_time)
-        _refresh(_get_apps(), start_time)
-
-    incomplete_refresh_start_time = db_steam_app_table_refreshes.get_incomplete_refresh_start_time()
-
-    if incomplete_refresh_start_time is not None:
-
-        apps = _get_apps()
-        last_app_id = db_steam_app_table_refreshes.get_last_app_id(incomplete_refresh_start_time)
-        index = 0
-
-        for i in range(len(apps)):
-            if apps[i]["appid"] == last_app_id:
-                index = i
-                break
-
-        _refresh(apps[index+1:], incomplete_refresh_start_time)
+        _fill_table(_get_apps())
 
     while is_running():
+        start_time = time()
+
         apps = _get_apps()
-
-        if time() - db_steam_app_table_refreshes.get_latest_refresh_start_time() > four_months:
-            start_time = int(time())
-            db_steam_app_table_refreshes.add(start_time)
-            _refresh(apps, start_time)
-
         app_ids = db_steam_apps.get_all_app_ids()
+        app_ids_in_need_of_update = db_steam_apps.get_oldest_by_update_time(150)
+
         new_apps = []
+        update_apps = []
         removed_from_steam = []
 
-        # check and add any apps that are new to steam
         for app in apps:
             if app["appid"] not in app_ids:
                 new_apps.append(app)
+            if app["appid"] in app_ids_in_need_of_update:
+                update_apps.append(app)
 
+        _fill_table(new_apps[:100] + update_apps)
 
+        for app_id in app_ids:
+            if not any(x["appid"] == app_id for x in apps):
+                removed_from_steam.append(app_id)
 
-        # remove apps that are no longer on steam
+        db_steam_apps.remove_all(removed_from_steam)
 
-        sleep(half_hour)
+        sleep(half_hour - (time()-start_time))
 
 
 threading.Thread(target=_background_apps_refresh, args=(lambda: _is_running,), daemon=True).start()
@@ -133,11 +109,18 @@ class CommandList(commands.CommandList):
     async def get_player_count(self, context, name: str):
         await context.defer_message()
 
-        app = db_steam_apps.search(name, _make_searchable_name(name))
+        apps = await context.run_blocking_func(db_steam_apps.search, name, _make_searchable_name(name))
 
-        if app is None:
-            await context.send_message(f"no steam games with the name *{name}*")
+        if len(apps) == 0:
+            await context.send_message(f"No steam games with the name *{name}*")
             return
+
+        index_of_highest_priority = 0
+        for i in range(len(apps)):
+            if apps[index_of_highest_priority]["search_priority"] < apps[i]["search_priority"]:
+                index_of_highest_priority = i
+
+        app = apps[index_of_highest_priority]
 
         request = requests.get(
             f"https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid={app['appid']}&format=json"
@@ -145,7 +128,8 @@ class CommandList(commands.CommandList):
         response = request.json()
 
         if "player_count" not in response["response"]:
-            await context.send_message(f"Error getting player count for *{app['name']}*")
+            await context.send_message(f"No player count is currently available for the app *{app['name']}*")
             return
 
-        await context.send_message(f"*{app['name']}* currently has {response['response']['player_count']} players online")
+        await context.send_message(
+            f"*{app['name']}* currently has {response['response']['player_count']} players online")
