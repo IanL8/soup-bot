d273562cade6253c33c90eb71b06c0629a6c03c2
Ian <ian.lommock@gmail.com>
2025-06-17 15:34:49 -0400

    Made a new file and table for storing active timers--this enables them to persist after program restarts.

--- database/database_management/db_timers.py
+++ database/database_management/db_timers.py
@@ -1,45 +1,45 @@
-from random import choice as _choice
-import time as _time
-
 from . import _soup_sql
-from soup_util.constants import FORTUNES as _FORTUNES
 
 
-def _duration_to_string(duration) -> str:
-    time_readout = ""
-
-    for seconds, unit in [(86400, "days"), (3600, "hours"), (60, "minutes"), (1, "seconds")]:
-        unit_amount = int(duration / seconds)
-        duration = duration % seconds
-        time_readout += f"{unit_amount} {unit}, " if unit_amount > 0 else ""
-
-    return time_readout[:-2]
-
-
-def fortune(user) -> str:
-    """Returns a random fortune, or time until next usage is available."""
+def add(uid, gid, name, channel_id, end_time) -> int:
+    """Adds a new timer. Returns the corresponding timer_id."""
 
     conn = _soup_sql.connect()
 
-    # don't report db errors to user - better to let the cooldown portion fail and still give them their fortune redeem
+    if not uid in _soup_sql.query(conn, "SELECT uid FROM Users WHERE gid=?;", (gid,)).values:
+        _soup_sql.add_member(conn, uid, gid)
 
-    if not user.id in _soup_sql.query(conn, "SELECT uid FROM Users WHERE gid=?;", (user.guild.id,)).values:
-        _soup_sql.add_member(conn, user.id, user.guild.id)
-
-    user_ids = _soup_sql.query(conn, "SELECT uid FROM UserTimers WHERE tid='fortune';").values
-    if user.id in user_ids:
-        last_usage_time = _soup_sql.query(conn, "SELECT start_time FROM UserTimers WHERE uid=?;", (user.id,)).values[0]
-    else:
-        _soup_sql.query(conn, "INSERT INTO UserTimers (tid, uid) VALUES ('fortune', ?);", (user.id,))
-        last_usage_time = 0
-
-    time_since_last_use = _time.time() - last_usage_time
-    if time_since_last_use < 72000:
-        conn.close()
-        return f"{_duration_to_string(72000 - time_since_last_use)} until next fortune redeem."
-
-    _soup_sql.query(conn, "UPDATE UserTimers SET start_time=? WHERE tid='fortune' AND uid=?;", (int(_time.time()), user.id))
+    timer_id = _soup_sql.query(
+        conn,
+        "INSERT INTO UserTimers (uid, name, channel_id, end_time) VALUES (?, ?, ?, ?) RETURNING timer_id;",
+        (uid, name, channel_id, end_time)
+    ).values[0]
 
     conn.commit()
     conn.close()
-    return _choice(_FORTUNES)
+
+    return timer_id
+
+
+def remove(timer_id):
+    """Remove a timer based on the timer_id."""
+
+    conn = _soup_sql.connect()
+
+    _soup_sql.query(conn, "DELETE FROM UserTimers WHERE timer_id=?;", (timer_id, ))
+
+    conn.commit()
+    conn.close()
+
+
+def get_all() -> [dict]:
+    """Returns a list of all active timers. Each element is a dictionary with the keys 'uid', 'name', 'channel_id',
+    'end_time', and 'timer_id'."""
+
+    conn = _soup_sql.connect()
+
+    values = _soup_sql.query(conn, "SELECT uid, name, channel_id, end_time, timer_id FROM UserTimers;", flatten_results=False).values
+
+    conn.close()
+    return [{"uid": v[0], "name": v[1], "channel_id": v[2], "end_time": v[3], "timer_id": v[4]} for v in values]
+
