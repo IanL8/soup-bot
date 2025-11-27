be989376eed7ba37df8b7b07c4162afa130de9e4
IanL8 <ian.lommock@gmail.com>
2025-11-26 20:34:43 -0500

    Essentially rewrote the entire thing to use lavalink. Made the queue and 'now playing' messages look much better.

--- command_lists/music.py
+++ command_lists/music.py
@@ -1,231 +1,262 @@
-import threading as _threading
+import re as _re
 import asyncio as _asyncio
+import discord as _discord
+import lavalink as _lavalink
+from lavalink import events as _events
 from math import ceil as _ceil
-from async_timeout import timeout as _timeout
 
 import command_management.commands as _commands
-from . import _music_support
+from soup_util import constants as _constants
+from . import _lavalink_voice_protocol
+from . import _ui_views
 
 
-_sessions: {str: _music_support.Session} = dict()
-_DISPLAY_LIMIT = 20
+_url_rx = _re.compile(r"https?://(?:www\.)?.+")
+
+_WAITING_TIME = 30
+
+_FORCE_DISCONNECT = 4014
+_RATE_LIMITED = 4021
+_CALL_DELETED = 4022
+
+_FIELD_LIMIT = 10
 
 
 class CommandList(_commands.CommandList):
 
     name = "music commands"
 
-    async def on_start(self):
-        _threading.Thread(target=_music_support.background_streaming, args=(lambda: _sessions,), daemon=True).start()
+    def __init__(self, *args):
+        super().__init__(*args)
 
-    async def on_close(self):
-        _sessions.clear()
+        self.lavalink = None
+
+    async def on_start(self):
+        if not hasattr(self.client, 'lavalink'):
+            self.client.lavalink = _lavalink.Client(self.client.user.id)
+
+            self.client.lavalink.add_node(
+                host=_constants.NODE1_HOST,
+                port=_constants.NODE1_PORT,
+                password=_constants.NODE1_PASSWORD,
+                region=_constants.NODE1_REGION,
+                name=_constants.NODE1_NAME
+            )
+            self.client.lavalink.add_node(
+                host=_constants.NODE2_HOST,
+                port=_constants.NODE2_PORT,
+                password=_constants.NODE2_PASSWORD,
+                region=_constants.NODE2_REGION,
+                name=_constants.NODE2_NAME
+            )
+
+        self.lavalink = self.client.lavalink
+        self.lavalink.add_event_hook(self._on_track_start, event=_events.TrackStartEvent)
+        self.lavalink.add_event_hook(self._on_queue_end, event=_events.QueueEndEvent)
+
+    @_lavalink.listener(_events.TrackStartEvent)
+    async def _on_track_start(self, event: _events.TrackStartEvent):
+        guild = self.client.get_guild(event.player.guild_id)
+
+        if not guild:
+            await self.lavalink.player_manager.destroy(event.player.guild_id)
+            return
+
+        channel = guild.get_channel(event.player.fetch("text-channel"))
+
+        if channel:
+            embed = _discord.Embed(
+                colour=_discord.Colour.dark_gold(),
+                title=event.track.title[:_ui_views.TITLE_CHAR_LIMIT],
+                description=event.track.author[:_ui_views.VALUE_CHAR_LIMIT]
+            )
+            embed.set_thumbnail(url=event.track.artwork_url)
+
+            await channel.send(f"Now playing...", embed=embed)
+
+    @_lavalink.listener(_events.QueueEndEvent)
+    async def _on_queue_end(self, event: _events.QueueEndEvent):
+        guild = self.client.get_guild(event.player.guild_id)
+
+        await _asyncio.sleep(_WAITING_TIME)
+
+        if not event.player.is_playing and guild is not None:
+            await guild.voice_client.disconnect(force=True)
+
+    @staticmethod
+    def _reset_player(player):
+        player.set_shuffle(False)
+        player.set_loop(0)
+
+    @staticmethod
+    async def _create_player(context):
+        if context.guild is None:
+            raise _commands.CommandError("This command must be used in a server.")
+
+        return context.bot.lavalink.player_manager.create(context.guild.id)
+
+    @staticmethod
+    async def _connect_to_voice(player, context):
+        if not context.author.voice or not context.author.voice.channel:
+            raise _commands.CommandError("You must join a voice channel to use this command.")
+
+        if context.guild.voice_client is None:
+            CommandList._reset_player(player)
+
+            permissions = context.author.voice.channel.permissions_for(context.guild.me)
+
+            if not permissions.connect or not permissions.speak:
+                raise _commands.CommandError("The bot needs `CONNECT` and `SPEAK` permissions.")
+
+            player.store("text-channel", context.channel.id)
+            await context.author.voice.channel.connect(cls=_lavalink_voice_protocol.LavalinkVoiceClient)
 
     @_commands.command("join", desc="Makes the bot join your voice channel")
     async def join(self, context):
-        if not context.author.voice:
-            await context.send_message("user is not in a voice channel")
-        else:
-            await context.author.voice.channel.connect()
-            await context.confirm()
+        player = await self._create_player(context)
+        await self._connect_to_voice(player, context)
+
+        await context.confirm()
 
     @_commands.command("leave", desc="Makes the bot leave its voice channel")
     async def leave(self, context):
         if not context.guild.voice_client in context.bot.voice_clients:
-            await context.send_message("bot is not in a voice channel")
+            raise _commands.CommandError("Bot is not in a voice channel.")
 
-        else:
-            if context.guild.id in _sessions.keys():
-                _sessions[context.guild.id].end()
-                _sessions.pop(context.guild.id)
+        player = await self._create_player(context)
 
-            await context.guild.voice_client.disconnect(force=False)
-            await context.confirm()
+        player.queue.clear()
+        await player.stop()
 
-    @_commands.command("play", desc="Plays audio from a yt/spotify url, or from a given song name in the voice channel")
+        await context.guild.voice_client.disconnect(force=False)
+        await context.confirm()
+
+    @_commands.command("play", desc="Plays music. Supports youtube/spotify/apple/soundcloud links or keyword searches")
     async def play(self, context, media: str):
-        in_vc = True
-
-        if not context.guild.voice_client in context.bot.voice_clients:
-            in_vc = False
-
-            if not context.author.voice:
-                await context.send_message("user is not in a voice channel")
-                return
-
+        player = await self._create_player(context)
+        await self._connect_to_voice(player, context)
         await context.defer_message()
 
-        if context.guild.id not in _sessions.keys():
-            _sessions[context.guild.id] = _music_support.Session()
+        if not _url_rx.match(media):
+            media = f"ytsearch:{media}"
 
-        tracks = _music_support.gather_tracks(media)
-        if not tracks:
-            await context.send_message("no tracks could be found from url")
-            return
+        result = await player.node.get_tracks(media)
 
-        elif not _sessions[context.guild.id].playing:
-            track = tracks.pop(0)
-
-            if not await _asyncio.to_thread(track.stream):
-                await context.send_message("bad url or search")
-                return
-            if not in_vc:
-                await context.author.voice.channel.connect()
-
-            _sessions[context.guild.id].all_tracks.append(track)
-            _sessions[context.guild.id].playing = True
-
-            await context.send_message(f"now playing...\n```{track.name}```")
-
-            context.guild.voice_client.play(
-                track.audio_source,
-                after=lambda _: _ if not _sessions.get(context.guild.id) else _sessions[context.guild.id].play_next(
-                    context.channel,
-                    context.guild.voice_client,
-                    context.bot.loop
-                )
-            )
-
+        if result.load_type == _lavalink.LoadType.EMPTY:
+            raise _commands.CommandError("No tracks found.")
+        elif result.load_type == _lavalink.LoadType.PLAYLIST:
+            for track in result.tracks:
+                track.extra["requester"] = context.author.id
+                player.add(track=track)
         else:
-            await context.confirm()
+            result.tracks[0].extra["requester"] = context.author.id
+            player.add(track=result.tracks[0])
 
-        if tracks and len(tracks) > 0:
-            _sessions[context.guild.id].add_tracks(tracks)
+        if not player.is_playing:
+            await player.play()
 
-    @_commands.command("shuffle", desc="Shuffles the current queue")
+        await context.confirm()
+
+    @_commands.command("shuffle", desc="Toggles on/off queue shuffle")
     async def shuffle(self, context):
-        if not context.guild.id in _sessions.keys():
-            await context.send_message("no session - play a track before using this command")
+        player = await self._create_player(context)
 
-        else:
-            _sessions[context.guild.id].shuffle()
-            await context.confirm()
+        if not player.is_playing:
+            raise _commands.CommandError("No track is currently playing.")
 
-    @_commands.command("always-shuffle", desc="Reshuffles the queue after every loop, if looping is enabled")
-    async def always_shuffle(self, context):
-        if not context.guild.id in _sessions.keys():
-            await context.send_message("no session - play a track before using this command")
+        shuffle = not player.shuffle
 
-        elif _sessions[context.guild.id].always_shuffle:
-            _sessions[context.guild.id].always_shuffle = False
-            await context.send_message("always shuffle disabled")
+        player.set_shuffle(shuffle)
+        await context.send_message(f"Shuffle {'on' if shuffle else 'off'}.")
 
-        else:
-            _sessions[context.guild.id].always_shuffle = True
-            await context.send_message("always shuffle enabled")
-
-    @_commands.command("loop", desc="Toggles looping the queue")
+    @_commands.command("loop-queue", desc="Toggles on/off queue loop")
     async def loop_queue(self, context):
-        if not context.guild.id in _sessions.keys():
-            await context.send_message("no session - play a track before using this command")
+        player = await self._create_player(context)
 
-        elif _sessions[context.guild.id].loop_all:
-            _sessions[context.guild.id].loop_all = False
-            await context.send_message("looping disabled")
+        if not player.is_playing:
+            raise _commands.CommandError("No track is currently playing.")
 
-        else:
-            _sessions[context.guild.id].loop_all = True
-            await context.send_message("looping enabled")
+        # 0 - off
+        # 1 - single track
+        # 2 - queue
+        loop = 2 if not player.loop == 2 else 0
+
+        player.set_loop(loop)
+        await context.send_message(f"Queue looping {'on' if loop == 2 else 'off'}.")
+
+    @_commands.command("loop-track", desc="Toggles on/off single track loop")
+    async def loop_track(self, context):
+        player = await self._create_player(context)
+
+        if not player.is_playing:
+            raise _commands.CommandError("No track is currently playing.")
+
+        loop = 1 if not player.loop == 1 else 0
+
+        player.set_loop(loop)
+        await context.send_message(f"Track looping {'on' if loop == 1 else 'off'}.")
 
     @_commands.command("pause", desc="Pause the current track")
     async def pause(self, context):
-        if not context.guild.voice_client in context.bot.voice_clients:
-            await context.send_message("bot is not in a voice channel")
+        player = await self._create_player(context)
 
-        elif not context.guild.voice_client.is_playing():
-            await context.send_message("there is no track playing")
+        if not player.is_playing:
+            raise _commands.CommandError("No track is currently playing.")
+        elif player.paused:
+            raise _commands.CommandError("The track is already paused.")
 
-        else:
-            context.guild.voice_client.pause()
-            await context.confirm()
+        await player.set_pause(True)
+        await context.confirm()
 
     @_commands.command("resume", desc="Resume the current track")
     async def resume(self, context):
-        if not context.guild.voice_client in context.bot.voice_clients:
-            await context.send_message("bot is not in a voice channel")
+        player = await self._create_player(context)
 
-        elif context.guild.voice_client.is_playing():
-            await context.send_message("the track is already playing")
+        if not player.is_playing:
+            raise _commands.CommandError("No track is currently playing.")
+        elif not player.paused:
+            raise _commands.CommandError("The track is already playing.")
 
-        else:
-            context.guild.voice_client.resume()
-            await context.confirm()
+        await player.set_pause(False)
+        await context.confirm()
 
     @_commands.command("skip", desc="Skip the current track")
     async def skip(self, context):
-        if not context.guild.voice_client in context.bot.voice_clients:
-            await context.send_message("bot is not in a voice channel")
-            return
+        player = await self._create_player(context)
 
-        context.guild.voice_client.stop()
+        if not player.is_playing:
+            raise _commands.CommandError("No track is currently playing.")
+
+        await player.skip()
         await context.confirm()
 
-    @_commands.command("queue", desc="Display the queue")
-    async def get_queue(self, context):
-        if not context.guild.id in _sessions.keys() or len(_sessions[context.guild.id].queue) == 0:
-            await context.send_message("empty queue")
-            return
+    @_commands.command("queue", desc="Displays the queue")
+    async def queue(self, context):
+        player = await self._create_player(context)
 
-        queue = [track for track in _sessions[context.guild.id].queue]
-        message = await context.send_message(self._page(queue[:_DISPLAY_LIMIT], 1))
+        if not player.is_playing:
+            raise _commands.CommandError("No track is currently playing.")
+        elif len(player.queue) == 0:
+            raise _commands.CommandError("The queue is empty.")
 
-        if len(queue) < _DISPLAY_LIMIT + 1:
-            return
+        tracks = [track for track in player.queue]
+        embeds = []
+        total_pages = _ceil(len(tracks) / _FIELD_LIMIT)
 
-        await message.add_reaction("◀️")
-        await message.add_reaction("▶️")
+        for i in range(total_pages):
+            embeds.append(_discord.Embed(colour=_discord.Colour.dark_gold(), title=f"Queue [{i+1}/{total_pages}]"))
+            embeds[i].set_image(url=_constants.EMBED_TRANSPARENT_IMAGE_URL)
 
-        async with _timeout(60):
+            for _ in range(len(tracks) if len(tracks) < _FIELD_LIMIT else _FIELD_LIMIT):
+                track = tracks.pop(0)
 
-            floor = 0
-            ceiling = _DISPLAY_LIMIT
+                embeds[i].add_field(
+                    name=track.title[:_ui_views.TITLE_CHAR_LIMIT],
+                    value=f"-# {track.author}"[:_ui_views.VALUE_CHAR_LIMIT],
+                    inline=False
+                )
 
-            while True:
-                try:
-                    # credit: https://stackoverflow.com/a/70661168
-                    tasks = [
-                        context.bot.loop.create_task(context.bot.wait_for('reaction_remove')),
-                        context.bot.loop.create_task(context.bot.wait_for('reaction_add'))
-                    ]
-                    done, pending = await _asyncio.wait(tasks,return_when=_asyncio.FIRST_COMPLETED)
-
-                except _asyncio.exceptions.CancelledError:
-                    return
-
-                if not done:
-                    continue
-
-                reaction, user = done.pop().result()
-
-                if user == context.bot.user or reaction.message != message:
-                    continue
-                elif reaction.emoji == "▶️":
-
-                    if len(queue) > ceiling:
-                        floor += _DISPLAY_LIMIT
-                        ceiling += _DISPLAY_LIMIT if len(queue) > _DISPLAY_LIMIT else len(queue)
-                    else: # len(queue) == ceiling:
-                        floor = 0
-                        ceiling = _DISPLAY_LIMIT
-
-                    await message.edit(content=self._page(queue[floor:ceiling], _ceil(ceiling / _DISPLAY_LIMIT)))
-
-                elif reaction.emoji == "◀️":
-
-                    if floor != 0:
-                        ceiling -= ceiling-floor
-                        floor -= _DISPLAY_LIMIT
-                    else: # floor == 0
-                        ceiling = len(queue)
-                        floor = ceiling - (_DISPLAY_LIMIT if (ceiling % _DISPLAY_LIMIT) == 0 else (ceiling % _DISPLAY_LIMIT))
-
-                    await message.edit(content=self._page(queue[floor:ceiling], _ceil(ceiling / _DISPLAY_LIMIT)))
-
-    @staticmethod
-    def _page(queue, page_number):
-        header = f"queue - page ({page_number}/{_ceil(len(queue) / _DISPLAY_LIMIT)})```\n" # TODO make pages easier to make for other commands
-        body = ""
-
-        for track in queue:
-            body += f"{track.name}\n"
-        return f"{header}{body}```"
+        if total_pages > 1:
+            await context.send_message("", embed=embeds[0], view=_ui_views.PagedEmbedView(embeds))
+        else:
+            await context.send_message("", embed=embeds[0])
