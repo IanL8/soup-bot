1648b0643538a8011a97b5bf3af131dfca2d7c62
IanL8 <ian.lommock@gmail.com>
2025-11-26 20:25:01 -0500

    Improved formatting. Added EMBED_TRANSPARENT_IMAGE_URL, NODE1 and NODE2 information, and WORDLE_LIST.

--- soup_util/constants.py
+++ soup_util/constants.py
@@ -14,18 +14,40 @@ def _read_json_file_to_dict(file_name):
 
 # env file
 _load_dotenv("resources/.env")
+
 TOKEN = _getenv("DISCORD_TOKEN")
+
 FFMPEG_EXE = _getenv("FFMPEG_EXE")
+
 SPOTIPY_CLIENT_SECRET = _getenv("SPOTIPY_CLIENT_SECRET")
 SPOTIPY_CLIENT_ID = _getenv("SPOTIPY_CLIENT_ID")
+
 AZURE_TRANSLATOR_SECRET_KEY = _getenv("AZURE_TRANSLATOR_KEY")
 AZURE_TRANSLATOR_REGION = _getenv("AZURE_TRANSLATOR_REGION")
+
 DEEPL_API_KEY = _getenv("DEEPL_API_KEY")
+
 STEAM_API_KEY = _getenv("STEAM_API_KEY")
+
 YOUTUBE_API_KEY = _getenv("YOUTUBE_API_KEY")
+
+EMBED_TRANSPARENT_IMAGE_URL = _getenv("EMBED_TRANSPARENT_IMAGE_URL")
+
+NODE1_HOST = _getenv("NODE1_HOST")
+NODE1_PORT = _getenv("NODE1_PORT")
+NODE1_PASSWORD = _getenv("NODE1_PASSWORD")
+NODE1_REGION = _getenv("NODE1_REGION")
+NODE1_NAME = _getenv("NODE1_NAME")
+
+NODE2_HOST = _getenv("NODE2_HOST")
+NODE2_PORT = _getenv("NODE2_PORT")
+NODE2_PASSWORD = _getenv("NODE2_PASSWORD")
+NODE2_REGION = _getenv("NODE2_REGION")
+NODE2_NAME = _getenv("NODE2_NAME")
 
 # txt files
 WORD_LIST = _read_file_to_list("resources/word_list.txt")
+WORDLE_LIST = _read_file_to_list("resources/wordle_list.txt")
 FORTUNES = _read_file_to_list("resources/fortunes.txt")
 
 # json files
