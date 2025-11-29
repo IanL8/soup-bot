ac5f6be6a71265bdeb9bff77aac5bd657fda14f1
IanL8 <ian.lommock@gmail.com>
2025-11-29 03:14:06 -0500

    Added back get_prefix() and set_prefix().

--- database/database_management/db_guilds.py
+++ database/database_management/db_guilds.py
@@ -1,6 +1,9 @@
 from . import _soup_sql
 
 
+_prefix_cache = dict()
+
+
 def add(guild):
     """Add a guild to the db."""
 
@@ -12,3 +15,26 @@ def add(guild):
     conn.commit()
     conn.close()
     return True
+
+def get_prefix(guild):
+    """Fetches the basic command prefix for this guild."""
+
+    if guild.id in _prefix_cache.keys():
+        return _prefix_cache[guild.id]
+
+    conn = _soup_sql.connect()
+
+    prefix = _soup_sql.query(conn, "SELECT flag FROM Guilds WHERE gid=?;", (guild.id,)).values[0]
+
+    _prefix_cache[guild.id] = prefix
+    return prefix
+
+def set_prefix(guild, new_prefix):
+    """Sets the prefix for the guild to new_prefix."""
+
+    conn = _soup_sql.connect()
+
+    _soup_sql.query(conn, "UPDATE Guilds SET flag=? WHERE gid=?;", (new_prefix, guild.id))
+
+    conn.commit()
+    _prefix_cache[guild.id] = new_prefix
