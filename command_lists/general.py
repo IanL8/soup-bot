8faf293003c3260eff65f7710ae0b36fd12c89f4
IanL8 <ian.lommock@gmail.com>
2025-12-27 14:52:24 -0500

    Switch to Context.edit_message() for updating timed out UI views

--- command_lists/general.py
+++ command_lists/general.py
@@ -126,11 +126,11 @@ class CommandList(_commands.CommandList):
 
         if total_pages > 1:
             view = _ui_views.PagedEmbedView(embeds)
-            message = await context.send_message("", embed=embeds[0], view=view)
+            await context.send_message("", embed=embeds[0], view=view)
 
             await view.wait()
             view.disable_items()
-            await message.edit(view=view)
+            await context.edit_message(view=view)
         else:
             await context.send_message("", embed=embeds[0])
 
